7e0f681649ea727df3c95812a51f7fac7d9c27d1
julain19 <92892705+julain19@users.noreply.github.com>
2026-07-16 20:01:50 +0200

    Add: sql-game-play-analysis-i - submission-4

---
+++ SQL/sql-game-play-analysis-i/submission-4.sql
@@ -0,0 +1,8 @@
+-- Write your query below
+SELECT player_id, event_date AS first_login
+FROM (
+    SELECT *, 
+           RANK() OVER(PARTITION BY player_id ORDER BY event_date) AS rnk
+    FROM activity
+)
+WHERE rnk = 1;
